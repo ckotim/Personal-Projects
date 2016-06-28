604bb40a5a0e46ec8f56f1146aa946a8761918c7
Christian <Christian@Christian-LT>
2016-06-28 16:19:19 -0600

    More work

--- src/Markov.java
+++ src/Markov.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -10,10 +11,7 @@ public class Markov {
 	private static ArrayList<String> words = new ArrayList<String>();
 	private static ArrayList<String> chain = new ArrayList<String>();
 
-
 	public static void main(String args[]) {
-
-		
 
 		Scanner scin = new Scanner(System.in);
 		System.out.print("Which file should be read? ");
@@ -25,15 +23,16 @@ public class Markov {
 		String first = scin.nextLine();
 		System.out.println("Enter the second word: ");
 		String second = scin.nextLine();
+		scin.close();
 		chain.add(first);
 		chain.add(second);
-		
+
 		for (int i = 0; i < numWords; i++) {
 			String temp = calculateNext(first, second, words);
 			chain.add(temp);
 			first = second;
 			second = temp;
-			
+
 		}
 		String finalChain = "";
 		for (int i = 0; i < chain.size(); i++) {
@@ -41,9 +40,8 @@ public class Markov {
 			finalChain += " ";
 		}
 		System.out.println(finalChain);
-		
 
-		// System.out.println(words.toString());
+		
 	}
 
 	private static void readFile(String fileName) {
@@ -53,13 +51,12 @@ public class Markov {
 			String temp;
 			while (sc.hasNext()) {
 				temp = sc.next();
-				//System.out.println(temp);
 				words.add(temp);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	private static String calculateNext(String first, String second, ArrayList<String> text) {
@@ -94,64 +91,13 @@ public class Markov {
 			return third;
 		}
 
-		//System.out.println(freq.toString());
-		
 		return returnHighestProbability(freq);
 	}
 
-	private static String returnHighest(ArrayList<String> possible){
-		Map<Integer, String> markovFreq = new TreeMap<Integer, String>();
-		int num = 0;
-		int max = 0;
-		
-		possible.sort(null);
-		String temp = possible.get(0);
-		
-		for (int i = 0; i < possible.size(); i++) {
-			if (temp.equals(possible.get(i))) {
-				num++;
-			}
-			else{
-				if (max < num) {
-					max = num;
-				}
-				markovFreq.put(num, possible.get(i - 1));
-				temp = possible.get(i);
-				num = 0;
-				i--;
-				continue;
-			}
-			if (i == possible.size() - 1) {
-				if (max < num) {
-					max = num;
-				}
-				markovFreq.put(num, possible.get(i));
-			}
-
-	
-			
-			
-			//System.out.println(markovFreq.toString());
-				
-		}
-		System.out.println(markovFreq.toString());
-		return markovFreq.get(max);
-	}
-	
-	private static String returnHighestProbability(ArrayList<String> possible){
-		possible.sort(null);
-		//System.out.println(possible);
-		int temp = (int)Math.round((Math.random() * (possible.size() - 1)));
-		//System.out.println(temp);
+	private static String returnHighestProbability(ArrayList<String> possible) {
+		int temp = (int) Math.round((Math.random() * (possible.size() - 1)));
+		Random rand = new Random(1000);
+		temp = Math.abs(rand.nextInt() % (possible.size()));
 		return possible.get(temp);
 	}
-	
-
-	
-	private static String returnFirst(ArrayList<String> possible){
-		possible.sort(null);
-		return possible.get(0);
-	}
-	
-
 }
